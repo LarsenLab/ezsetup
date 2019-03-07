ea646fc4216dd51de496488debdf7e4a41be8504
Joan Zhang <44436425+xzhan50@users.noreply.github.com>
2019-03-06 19:15:27 -0500

    use map() to create folders and readme files

--- R/setRproj.R
+++ R/setRproj.R
@@ -11,6 +11,7 @@
 #'
 #' @import
 #' devtools
+#' purrr
 #' @examples
 #' \dontrun{
 #' setProj("/Users/newfolder")
@@ -18,10 +19,15 @@
 
 setRproj <- function(fd_name)
 {
-  # step 1 : creat the director if it doesn't exist
-  if(!dir.exists(fd_name)){
-    dir.create(fd_name,recursive = TRUE)
-  }
+  # step 1: create the main folder, sub-folders, and readme.txt under each folder
+    fd_list <- list( "f0" = fd_name,
+                  "f1" = paste(fd_name,"data",sep = "/"),
+                  "f2" = paste(fd_name,"data/raw",sep = "/"),
+                  "f3" = paste(fd_name,"output",sep = "/"),
+                  "f4" = paste(fd_name,"docs",sep = "/"))
+
+  purrr::map(fd_list,dir.create)
+  purrr::map(paste(fd_list,"readme.txt",sep = "/"),file.create)
 
   # step 2: create .Rproj file from devtools package, and make a copy with the subfolder name
   path <- file.path(fd_name, paste0(gsub(".*/","",fd_name), ".Rproj"))
@@ -29,22 +35,7 @@ setRproj <- function(fd_name)
                                package = "devtools")
   file.copy(template_path, path)
 
-  # step 3: setup folder structure
-  file.create(paste(fd_name,"readme.txt",sep="/"))
-
-  dir.create(paste(fd_name,"data",sep = "/"))
-  file.create(paste(paste(fd_name,"data",sep = "/"),"readme.txt",sep="/"))
-
-  dir.create(paste(fd_name,"data/raw",sep = "/"))
-  file.create(paste(paste(fd_name,"data/raw",sep = "/"),"readme.txt",sep="/"))
-
-  dir.create(paste(fd_name,"output",sep = "/"))
-  file.create(paste(paste(fd_name,"output",sep = "/"),"readme.txt",sep="/"))
-
-  dir.create(paste(fd_name,"docs",sep = "/"))
-  file.create(paste(paste(fd_name,"docs",sep = "/"),"readme.txt",sep="/"))
-
-  # step 4: setup initial rmd files
+  # step 3: setup initial rmd files
   fd_from <- paste(path.package("ezsetup"),"rmarkdown/templates/report/skeleton",sep="/")
   list_of_files <- list.files(fd_from, ".Rmd")
   file.copy(file.path(fd_from,list_of_files), fd_name)
